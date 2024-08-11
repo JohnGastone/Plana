a5d920c82ecd8c2e1e40ca186fc72906d8c0ae4e
JohnGastone <johngastone11@gmail.com>
2024-08-11 21:52:28 +0300

    App Bar

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,53 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Container(
+                height: 60,
+                width: 65,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                        image: AssetImage("./assets/amazed.jpeg"),
+                        fit: BoxFit.cover)),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hi, Kipwiru",
+                    style: GoogleFonts.poppins(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("04 Oct 2014",
+                      style: GoogleFonts.poppins(
+                          fontSize: 16, color: Colors.grey)),
+                ],
+              ),
+              SizedBox(
+                width: 100,
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
