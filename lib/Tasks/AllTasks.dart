6f2e6adec139f1a24a37650d75257852065c3a33
JohnGastone <johngastone11@gmail.com>
2024-08-30 23:09:22 +0300

    ++

--- lib/Tasks/AllTasks.dart
+++ lib/Tasks/AllTasks.dart
@@ -65,7 +65,78 @@ class _AlltasksPageState extends State<AlltasksPage> {
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(),
+          child: Column(
+            children: [
+              Container(
+                height: 150,
+                width: 340,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(22),
+                  color: Color.fromARGB(255, 151, 197, 198),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 20,
+                            child: Center(
+                              child: Image.asset(
+                                "assets/ui.png",
+                                height: 20,
+                                width: 20,
+                                color: Color.fromARGB(255, 95, 209, 211),
+                              ),
+                            ),
+                          ),
+                          Icon(Icons.more_vert)
+                        ],
+                      ),
+                      Text(
+                        "Python Pandas",
+                        style: GoogleFonts.poppins(
+                            color: Colors.white, fontSize: 16),
+                      ),
+                      SizedBox(
+                        height: 9,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: LinearProgressIndicator(
+                            value: 0.65,
+                            backgroundColor:
+                                const Color.fromARGB(255, 216, 213, 213),
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Progress",
+                            style: GoogleFonts.poppins(
+                                color: Colors.white, fontSize: 14),
+                          ),
+                          Text(
+                            "65%",
+                            style: GoogleFonts.poppins(
+                                color: Colors.white, fontSize: 14),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
