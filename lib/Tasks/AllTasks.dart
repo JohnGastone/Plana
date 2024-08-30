ca57f621ca94e8086c2d863ed3bb2d421e9393e0
JohnGastone <johngastone11@gmail.com>
2024-08-30 22:06:55 +0300

    ++

--- lib/Tasks/AllTasks.dart
+++ lib/Tasks/AllTasks.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlltasksPage extends StatefulWidget {
   const AlltasksPage({super.key});
@@ -12,7 +14,42 @@ class AlltasksPage extends StatefulWidget {
 class _AlltasksPageState extends State<AlltasksPage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            InkWell(
+              child: Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 233, 228, 228),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Icon(
+                    CupertinoIcons.back,
+                    color: theme.brightness == Brightness.light
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                ),
+              ),
+              onTap: () => Navigator.pop(context),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Text(
+              "Task Details",
+              style: GoogleFonts.poppins(
+                  fontSize: 22, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(),
