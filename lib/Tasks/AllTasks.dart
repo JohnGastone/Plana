ba07ec75efe6c56d4de6dbc6c798f84ca943deb5
JohnGastone <johngastone11@gmail.com>
2024-08-30 22:05:44 +0300

    ++

---
+++ lib/Tasks/AllTasks.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AlltasksPage extends StatefulWidget {
+  const AlltasksPage({super.key});
+
+  @override
+  State<AlltasksPage> createState() => _AlltasksPageState();
+}
+
+class _AlltasksPageState extends State<AlltasksPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
