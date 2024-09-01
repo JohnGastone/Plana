f27ab6d174cc91e31ee44b265988252a740b57e2
JohnGastone <johngastone11@gmail.com>
2024-09-01 17:45:36 +0300

    ++

---
+++ lib/models.dart
@@ -0,0 +1,22 @@
+class TaskModel {
+  String? name;
+  String? type;
+
+  TaskModel({this.name, this.type});
+}
+
+class TaskModelData {
+  static List<TaskModel> getTasks = [
+    TaskModel(name: 'Task 1', type: 'Type 1'),
+    TaskModel(name: 'Task 2', type: 'Type 2'),
+    TaskModel(name: 'Task 3', type: 'Type 3'),
+    TaskModel(name: 'Task 4', type: 'Type 4'),
+    TaskModel(name: 'Task 5', type: 'Type 5'),
+    TaskModel(name: 'Task 6', type: 'Type 6'),
+    TaskModel(name: 'Task 7', type: 'Type 7'),
+    TaskModel(name: 'Task 8', type: 'Type 8'),
+    TaskModel(name: 'Task 9', type: 'Type 9'),
+    TaskModel(name: 'Task 10', type: 'Type 10'),
+    TaskModel(name: 'Task 11', type: 'Type 11'),
+  ];
+}
