648e087a38b2fd39d86343ae16317f46443d73c9
JohnGastone <johngastone11@gmail.com>
2024-09-02 21:15:06 +0300

    ++

--- lib/models.dart
+++ lib/models.dart
@@ -7,7 +7,7 @@ class TaskModel {
 
 class TaskModelData {
   static List<TaskModel> getTasks = [
-    TaskModel(name: 'Task 1', type: 'Type 1'),
+    TaskModel(name: 'Weekly Calendar', type: 'Type 1'),
     TaskModel(name: 'Task 2', type: 'Type 2'),
     TaskModel(name: 'Task 3', type: 'Type 3'),
     TaskModel(name: 'Task 4', type: 'Type 4'),
