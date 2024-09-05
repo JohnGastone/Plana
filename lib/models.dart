76835b2822cf634e9aee1d00f5e42e703ff4692a
JohnGastone <johngastone11@gmail.com>
2024-09-05 22:10:50 +0300

    ++

--- lib/models.dart
+++ lib/models.dart
@@ -11,9 +11,6 @@ class TaskModelData {
     TaskModel(name: 'Task 2', type: 'Type 2'),
     TaskModel(name: 'Task 3', type: 'Type 3'),
     TaskModel(name: 'Task 4', type: 'Type 4'),
-    TaskModel(name: 'Task 5', type: 'Type 5'),
-    TaskModel(name: 'Task 6', type: 'Type 6'),
-    TaskModel(name: 'Task 7', type: 'Type 7'),
-    TaskModel(name: 'Task 8', type: 'Type 8'),
+    TaskModel(name: 'Task 5', type: 'Type 5')
   ];
 }
